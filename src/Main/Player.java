292d56029afbc002f52bf5b306cf7cf9be34653d
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-13 15:57:00 -0500

    added game object class

---
+++ src/Main/Player.java
@@ -0,0 +1,20 @@
+package Main;
+
+import java.awt.*;
+
+public class Player extends GameObject{
+
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+    }
+
+    @Override
+    public void tick() {
+
+    }
+
+    @Override
+    public void render(Graphics g) {
+
+    }
+}
