1731ecc2e0ffbdaf04d7c471e685b59392a0dc35
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-13 14:43:23 -0500

    got init window opening

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -1,8 +1,18 @@
 package Main;
 
 import java.awt.*;
+import java.io.Serial;
 
 public class Game extends Canvas implements Runnable{
+
+    @Serial
+    private static final long serialVersionUID = 1550691097823471818L;
+
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;
+
+    public Game() {
+        new Window(WIDTH, HEIGHT, "Wave game", this);
+    }
 
     public synchronized void start() {
 
@@ -12,9 +22,7 @@ public class Game extends Canvas implements Runnable{
 
     }
 
-
-
     public static void main(String[] args) {
-
+        new Game();
     }
 }
--- src/Main/Window.java
+++ src/Main/Window.java
@@ -11,6 +11,7 @@ public class Window extends Canvas {
 
     public Window(int width, int height, String title, Game game) {
         JFrame frame = new JFrame(title);
+
         frame.setPreferredSize(new Dimension(width, height));
         frame.setMaximumSize(new Dimension(width, height));
         frame.setMinimumSize(new Dimension(width, height));
