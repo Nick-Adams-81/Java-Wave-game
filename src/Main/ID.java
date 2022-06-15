34b87dc2a923516b83d1745c0251d12ecd95689e
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-14 22:57:16 -0500

    added fast enemy

--- src/Main/ID.java
+++ src/Main/ID.java
@@ -3,5 +3,6 @@ package Main;
 public enum ID {
     Player(),
     Trail(),
-    BasicEnemy();
+    BasicEnemy(),
+    FastEnemy();
 }
--- src/Main/Spawn.java
+++ src/Main/Spawn.java
@@ -16,13 +16,19 @@ public class Spawn {
 
     public  void tick() {
         scoreKeep++;
-        if(scoreKeep >= 1000) {
+        if(scoreKeep >= 250) {
             scoreKeep = 0;
             hud.setLevel(hud.getLevel() + 1);
 
-//            if(hud.getLevel() == 2) {
-                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT), ID.BasicEnemy, handler));
-//            }
+            if(hud.getLevel() == 2) {
+                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+            } else if(hud.getLevel() == 3) {
+                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+                handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+
+            } else if(hud.getLevel() == 4) {
+                handler.addObject(new FastEnemy(r.nextInt(Game.WIDTH - 50), r.nextInt(Game.HEIGHT -50), ID.FastEnemy, handler));
+            }
         }
     }
 }
