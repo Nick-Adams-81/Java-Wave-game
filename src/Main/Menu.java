9c83215b43f24d279a3886cfa0d884441cd93eb0
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-20 16:52:23 -0500

    created menu ui

--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -20,13 +20,28 @@ public class Menu extends MouseAdapter {
     }
 
     public void render(Graphics g) {
+        Font font = new Font("arial", 1, 50);
+        Font font2 = new Font("arial", 1, 50);
+
+        g.setFont(font);
+        g.setColor(Color.WHITE);
+        g.drawString("Menu", 400, 150);
+
+
         g.setColor(Color.WHITE);
         g.drawRect(370, 200, 200, 64);
+        g.setColor(Color.GREEN);
+        g.drawString("Play", 420, 247);
+
 
         g.setColor(Color.WHITE);
         g.drawRect(370, 300, 200, 64);
+        g.setColor(Color.YELLOW);
+        g.drawString("Help", 420, 347);
 
         g.setColor(Color.WHITE);
         g.drawRect(370, 400, 200, 64);
+        g.setColor(Color.RED);
+        g.drawString("Quit", 420, 447);
     }
 }
