fa205db8626da440807e63aad2a5d955ed1fe9a7
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-22 17:07:29 -0500

    added diff menu

--- src/Main/Game.java
+++ src/Main/Game.java
@@ -15,6 +15,8 @@ public class Game extends Canvas implements Runnable {
     private boolean running = false;
 
     public static boolean pause = false;
+    public int diff = 0;
+
 
     private Random r;
     private Handler handler;
@@ -26,6 +28,7 @@ public class Game extends Canvas implements Runnable {
 
     public enum STATE {
         Menu,
+        Select,
         Help,
         Game,
         End
@@ -120,7 +123,7 @@ public class Game extends Canvas implements Runnable {
                 }
             }
 
-        } else if(gameState == STATE.Menu || gameState == STATE.End) {
+        } else if(gameState == STATE.Menu || gameState == STATE.End || gameState == STATE.Select) {
             menu.tick();
             handler.tick();
         }
@@ -145,7 +148,7 @@ public class Game extends Canvas implements Runnable {
 
         if(gameState == STATE.Game) {
             hud.render(g);
-        } else if(gameState == STATE.Menu || gameState == STATE.Help || gameState == STATE.End) {
+        } else if(gameState == STATE.Menu || gameState == STATE.Help || gameState == STATE.End || gameState == STATE.Select) {
             menu.render(g);
         }
 
--- src/Main/Menu.java
+++ src/Main/Menu.java
@@ -25,15 +25,44 @@ public class Menu extends MouseAdapter {
 
         // play button
         if(mouseOver(mx, my, 370, 200, 200, 64)) {
-            game.gameState = Game.STATE.Game;
-            handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
-            handler.clearEnemys();
-            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+//            game.gameState = Game.STATE.Game;
+//            handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
+//            handler.clearEnemys();
+//            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+            game.gameState = Game.STATE.Select;
+            return;
         }
 
         // help button
         if(mouseOver(mx, my, 370, 300, 200, 64)) {
             game.gameState = Game.STATE.Help;
+        }
+
+        if(game.gameState == Game.STATE.Select) {
+            // normal button
+            if(mouseOver(mx, my, 370, 250, 200, 64)) {
+            game.gameState = Game.STATE.Game;
+            handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
+            handler.clearEnemys();
+            handler.addObject(new BasicEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+
+            game.diff = 0;
+            }
+
+            // hard button
+            if(mouseOver(mx, my, 370, 350, 200, 64)) {
+                game.gameState = Game.STATE.Game;
+                handler.addObject(new Player(Game.WIDTH/2-32, Game.HEIGHT/2-32, ID.Player, handler));
+                handler.clearEnemys();
+                handler.addObject(new HardEnemy(r.nextInt(Game.WIDTH), r.nextInt(Game.HEIGHT -50), ID.BasicEnemy, handler));
+
+                game.diff = 1;
+            }
+            // back button
+            if(mouseOver(mx, my, 370, 450, 200, 64)) {
+                game.gameState = Game.STATE.Menu;
+                return;
+            }
         }
 
         // back button for help
@@ -48,6 +77,8 @@ public class Menu extends MouseAdapter {
         if(mouseOver(mx, my, 370, 400, 200, 64)) {
             System.exit(1);
         }
+
+
 
         // try again button
         if(Game. gameState == Game.STATE.End) {
@@ -134,6 +165,30 @@ public class Menu extends MouseAdapter {
             g.setFont(font);
             g.drawRect(300, 600, 320, 64);
             g.drawString("Try Again?", 340, 648);
+        } else if(Game.gameState == Game.STATE.Select) {
+
+            Font font = new Font("arial", 2, 50);
+
+            g.setFont(font);
+            g.setColor(Color.CYAN);
+            g.drawString("Select Difficulty", 305, 150);
+
+
+            g.setColor(Color.WHITE);
+            g.drawRect(370, 250, 200, 64);
+            g.setColor(Color.GREEN);
+            g.drawString("Normal", 390, 297);
+
+
+            g.setColor(Color.WHITE);
+            g.drawRect(370, 350, 200, 64);
+            g.setColor(Color.YELLOW);
+            g.drawString("Hard", 420, 397);
+
+            g.setColor(Color.WHITE);
+            g.drawRect(370, 450, 200, 64);
+            g.setColor(Color.RED);
+            g.drawString("Back", 420, 497);
         }
     }
 }
