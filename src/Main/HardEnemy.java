23df253d60dcea6af3172f38fb15f8d20cdad0d2
Nick Adams <nick.pappas.adams@gmail.com>
2022-06-22 19:00:33 -0500

    re sized hard enemy

--- src/Main/HardEnemy.java
+++ src/Main/HardEnemy.java
@@ -28,13 +28,13 @@ public class HardEnemy extends GameObject {
 
         if(y <= 0 || y >= Game.HEIGHT - 32) {if(velY < 0) velY = -(r.nextInt(7) + 1) * -1; else  velY = (r.nextInt(7) + 1) * -1;}
         if(x <= 0 || x >= Game.WIDTH - 16) {if(velX < 0) velX = -(r.nextInt(7) + 1) * -1; else  velX = (r.nextInt(7) + 1) * -1;}
-        handler.addObject(new Trail((int)x, (int)y,  ID.Trail, Color.YELLOW, 16, 16, 0.02f, handler));
+        handler.addObject(new Trail((int)x, (int)y,  ID.Trail, Color.YELLOW, 32, 32, 0.02f, handler));
 
     }
 
     @Override
     public void render(Graphics g) {
         g.setColor(Color.YELLOW);
-        g.fillRect((int)x, (int)y, 16, 16);
+        g.fillRect((int)x, (int)y, 32, 32);
     }
 }
